d3ab7a1ce63a5ccf311140c093f6e1591c3095d3
mitoza <mitoza@gmail.com>
2021-10-11 19:17:05 +0300

    Add SFML Examples

---
+++ 02_DataStucrure/README.md
@@ -0,0 +1,38 @@
+# data_structure
+Effectively organisation of data in the computer
+
+Array
+
+LickedList
+
+Stack
+
+Queue
+
+BinaryTree
+
+BinarySearchTree
+
+Map
+
+Set
+
+Heep
+
+Hashing
+
+Graph
+
+Martix
+
+Trie
+
+SegmentTree
+
+SuffixTree
+
+
+
+[DataStructure] - Data Structure Schema
+
+  [DataStructure]: <https://www.geeksforgeeks.org/data-structures/>
--- 03_SFML/01_Form/main.cpp
+++ 03_SFML/01_Form/main.cpp
@@ -3,6 +3,7 @@
 // Source: https://habr.com/ru/post/248153/
 #include <iostream>
 
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
 const sf::Color white = sf::Color(255,255,255,255);
@@ -15,6 +16,11 @@ int main(int argc, char** argv) {
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
 
+  sf::Music music;
+  if (music.openFromFile("c:\\cpp_tutuorial\\03_SFML\\bin\\Debug\\music.ogg")) {
+    music.play();
+  }
+
   while (window.isOpen())
   {
       sf::Event event;
@@ -23,7 +29,10 @@ int main(int argc, char** argv) {
           if (event.type == sf::Event::Closed)
               window.close();
       }
+
+      // Clear screen
       window.clear();
+
       window.draw(shape);
       //window.draw(image);
       window.display();
